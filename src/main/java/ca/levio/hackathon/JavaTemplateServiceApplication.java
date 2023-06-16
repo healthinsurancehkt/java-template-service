f4e0455096a1f2cee6a5a1f779ddf2fb9f9c8e0c
Romero Pereira <romero.pereira@levio.ca>
2023-06-16 16:47:54 -0400

    Initial commit

---
+++ src/main/java/ca/levio/hackathon/JavaTemplateServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.levio.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTemplateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTemplateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/levio/hackathon/JavaTemplateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.levio.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTemplateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
