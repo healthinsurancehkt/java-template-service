a40103b08d132d391267e6048ee28d3a4bb6962e
indrashekargd <indrashekargr@gmail.com>
2023-06-20 14:59:00 -0400

    Integrated changes (#3)
    * Integrated changes
    
    * observation fix

--- src/main/java/ca/levio/hackathon/controller/WebFluxExampleController.java
+++ src/main/java/ca/levio/hackathon/controller/WebFluxExampleController.java
@@ -2,6 +2,8 @@ package ca.levio.hackathon.controller;
 
 import ca.levio.hackathon.model.Employee;
 import ca.levio.hackathon.service.WebFluxExampleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,22 +14,26 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/webflux-example")
+@Tag(name = "Employee Dashboard")
 public class WebFluxExampleController {
 
     private final WebFluxExampleService webFluxExampleService;
 
     @PostMapping("/create")
+    @Operation(summary = "Create new Employee")
     public ResponseEntity<Mono<Employee>> createEmployee(@RequestBody Employee employee){
         Mono<Employee> e = webFluxExampleService.createEmployee(employee);
         return new ResponseEntity<>(e, HttpStatus.OK);
     }
     @GetMapping("/findById/{id}")
+    @Operation(summary = "Fetch Employee data by ID")
     public ResponseEntity<Mono<Employee>> getEmployeeById(@PathVariable("id") Long idEmployee){
         Mono<Employee> e = webFluxExampleService.getEmployeeById(idEmployee);
         return new ResponseEntity<>(e, HttpStatus.OK);
     }
 
     @GetMapping("/findAll")
+    @Operation(summary = "Fetch all Employee Data")
     public ResponseEntity<Flux<Employee>> getAllEmployees(){
         Flux<Employee> e = webFluxExampleService.getAllEmployees();
         return new ResponseEntity<>(e, HttpStatus.OK);
