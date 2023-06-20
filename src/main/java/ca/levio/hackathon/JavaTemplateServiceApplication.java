fb1579cb1da422edd8ed2cb1c0aff820a86e058a
indrashekargd <indrashekargr@gmail.com>
2023-06-20 13:37:29 -0400

    Integrated changes

--- src/main/java/ca/levio/hackathon/JavaTemplateServiceApplication.java
+++ src/main/java/ca/levio/hackathon/JavaTemplateServiceApplication.java
@@ -2,7 +2,13 @@ package ca.levio.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(
+		description = "Demo API",
+		version = "1.0"
+))
 @SpringBootApplication
 public class JavaTemplateServiceApplication {
 
